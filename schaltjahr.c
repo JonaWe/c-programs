49de3d80fe5e6a152e52b9f44e92c5c71b4f748d
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-25 19:32:40 +0200

    adds comments to schaltjahr.c

--- schaltjahr.c
+++ schaltjahr.c
@@ -6,17 +6,22 @@ int
 main (int argc, char *argv[]) {
   int year;
 
+  // arg provided -> reading args
   if (argc > 1) {
     year = atoi(argv[1]);
-  } else {
+  }
+  // no arg provided -> reading from stdin
+  else {
+    printf("Bitte Jahr eingeben: ");
     scanf("%d", &year);
   }
 
+  // year % 4 == 0 && (year % 100 == 0 => year % 400 == 0)
   if (year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0)) {
     printf("%d ist ein Schaltjahr.\n", year);
   } else {
     printf("%d ist kein Schaltjahr.\n", year);
   }
 
-  return 0;
+  exit(0);
 }
