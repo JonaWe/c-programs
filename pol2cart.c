1a1ab8899da7d77ee1d7fef4a597342cb1c37450
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-25 19:54:27 +0200

    adds pol2cart.c

---
+++ pol2cart.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+struct vec2d {
+  double x;
+  double y;
+};
+
+struct vec2d
+pol2cart(double r, double phi) {
+  struct vec2d *tmp;
+  tmp = malloc(sizeof(struct vec2d));
+  tmp->x = r * cos(phi);
+  tmp->y = r * sin(phi);
+  return *tmp;
+}
+
+int main() {
+  struct vec2d result = pol2cart(1.4, 1.2);
+
+  printf("x=%f\n", result.x);
+  printf("y=%f\n", result.y);
+
+  exit(0);
+}
