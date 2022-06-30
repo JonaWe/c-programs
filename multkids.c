5c4cc8a039a689ee72097e4ab066070d8dfccf77
Jona Wessendorf <jonawessendorf@gmail.com>
2022-06-30 13:15:52 +0200

    adds multkids.c

---
+++ multkids.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("usage multkids <number>\n");
+        exit(-1);
+    }
+
+    int childs = atoi(argv[1]);
+
+    printf("Creating %d child processes\n", childs);
+
+    for (int i = 0; i < childs; i++) {
+        pid_t pid;
+
+        printf("Before the fork\n");
+        pid = fork();
+
+        if (pid == 0) {
+            printf("I'm a child\n");
+            break;
+        } else {
+            printf("I'm the parent and I created a child with pid = %d\n", pid);
+        }
+    }
+
+
+    exit(0);
+}
