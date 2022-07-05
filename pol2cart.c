07feb1e8665a75e1384bee3d9c50a10e2e910008
Jona <jona@Jona-Surface.localdomain>
2022-07-05 12:30:32 +0200

    pol2cart now returns the pointer instead of a copy of the object

--- pol2cart.c
+++ pol2cart.c
@@ -7,20 +7,21 @@ struct vec2d {
   double y;
 };
 
-struct vec2d
+struct vec2d*
 pol2cart(double r, double phi) {
   struct vec2d *tmp;
   tmp = malloc(sizeof(struct vec2d));
   tmp->x = r * cos(phi);
   tmp->y = r * sin(phi);
-  return *tmp;
+  return tmp;
 }
 
 int main() {
-  struct vec2d result = pol2cart(1.4, 1.2);
+  struct vec2d* result = pol2cart(1.4, 1.2);
 
-  printf("x=%f\n", result.x);
-  printf("y=%f\n", result.y);
+  printf("x=%f\n", result->x);
+  printf("y=%f\n", result->y);
 
+  free(result);
   exit(0);
 }
