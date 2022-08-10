71ada830def407fc820b9a87606f5fdbf35c0b44
Jona Wessendorf <jonawessendorf@gmail.com>
2022-08-10 16:03:53 +0200

    phase 1 working

--- abgabe/knn_jowessendorf.c
+++ abgabe/knn_jowessendorf.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <stddef.h>
 #include <pthread.h>
+#include <math.h>
+
+#define COND_ERR(cond, err)   \
+    if (cond)                 \
+    {                         \
+        fprintf(stderr, err); \
+        exit(-1);             \
+    }
 
 /*
  * This is my dll implementation.
@@ -123,7 +131,7 @@ void *worker_function(void *args)
         task->result = result;
 
         pthread_mutex_lock(&finished_queue_mutex);
-        list_add_tail(current, finished_task_queue);
+        list_add_tail(&current->head, &finished_task_queue->head);
         pthread_mutex_unlock(&finished_queue_mutex);
 
         pthread_cond_signal(&finished_queue_cond);
@@ -210,22 +218,135 @@ void thread_pool_shutdown(thread_pool_t *thread_pool)
     pthread_cond_destroy(&finished_queue_cond);
 }
 
+typedef struct vector_t
+{
+    int class;
+    int size;
+    double coordinates[];
+} vector_t;
+
+typedef struct interval_t
+{
+    int lower;
+    int upper;
+} interval_t;
+
+typedef struct neighbor_t
+{
+    int index;
+    double distance;
+} neighbor_t;
+
+long n;
+long n_max;
+int k_max;
+int b;
+int dimension;
+int classes;
+
+void read_input(char *filename, int n, vector_t **data)
+{
+    FILE *file;
+
+    file = fopen(filename, "r");
+
+    COND_ERR(file == NULL, "ERROR: File not found!\n")
+    COND_ERR(fscanf(file, "%ld %d %d", &n_max, &dimension, &classes) != 3, "ERROR: Invalid input file!\n")
+    COND_ERR(n > n_max, "ERROR: n is too big for the input!\n")
+
+    for (int i = 0; i < n; i++)
+    {
+        data[i] = malloc(sizeof(vector_t) + dimension * sizeof(double));
+        data[i]->size = dimension;
+        for (int j = 0; j < dimension; j++)
+        {
+            COND_ERR(fscanf(file, "%lf", &data[i]->coordinates[j]) != 1, "ERROR: Invalid input file!\n")
+        }
+        COND_ERR(fscanf(file, "%d", &data[i]->class) != 1, "ERROR: Invalid input file!\n")
+    }
+}
+
+double calc_distance(vector_t *v1, vector_t *v2)
+{
+    double distance = 0;
+    for (int i = 0; i < v1->size; i++)
+        distance += pow(v1->coordinates[i] - v2->coordinates[i], 2);
+    return distance;
+}
+
+int *find_k_nearest_neighbors(vector_t **data, vector_t *vector, int k, interval_t *blocks, int current_block_index)
+{
+    neighbor_t *nearest_neighbors = malloc(sizeof(neighbor_t) * k);
+    for (int i = 0; i < k; i++)
+    {
+        nearest_neighbors[i].index = -1;
+        nearest_neighbors[i].distance = -1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (blocks[current_block_index].lower <= i && i <= blocks[current_block_index].upper)
+            continue;
+        double d = calc_distance(vector, data[i]);
+        for (int j = 0; j < k; j++)
+            if (nearest_neighbors[j].index == -1 || d < nearest_neighbors[j].distance)
+            {
+                for (int l = k - 1; l > j; l--)
+                    nearest_neighbors[l] = nearest_neighbors[l - 1];
+                nearest_neighbors[j].index = i;
+                nearest_neighbors[j].distance = d;
+                break;
+            }
+    }
+    int *indexes = malloc(sizeof(int) * k);
+    for (int i = 0; i < k; i++)
+        indexes[i] = nearest_neighbors[i].index;
+    return indexes;
+}
+
 int main(int argc, char **argv)
 {
-    if (argc != 6)
-    {
-        fprintf(stderr, "Incorrect usage: knn_jowessendorf <filename> <n> <k_max> <b> <n_threads>\n");
-    }
+    COND_ERR(argc != 6, "ERROR: Incorrect usage: knn_jowessendorf <filename> <n> <k_max> <b> <n_threads>\n")
+
     // filename
     char *filename = argv[1];
     // n
-    long n = atol(argv[2]);
+    n = atol(argv[2]);
     // max k
-    int k_max = atoi(argv[3]);
+    k_max = atoi(argv[3]);
     // block size
-    int b = atoi(argv[4]);
+    b = atoi(argv[4]);
     // amount of threads
     int n_threads = atoi(argv[5]);
+
+    vector_t *data[n];
+
+    read_input(filename, n, data);
+
+    // printf("%lf %lf %lf\n", data[1]->coordinates[0], data[1]->coordinates[1], data[1]->coordinates[2]);
+
+    int rem = n % b;
+    int block_size = (n - rem) / b;
+    interval_t block_indices[b];
+    int shift = 0;
+    for (int i = 0; i < b; i++)
+    {
+        block_indices[i].lower = i * block_size + shift;
+        if (rem > 0)
+        {
+            rem--;
+            shift++;
+        }
+        block_indices[i].upper = (i + 1) * block_size + shift - 1;
+    }
+
+    for (int i = 0; i < b; i++)
+        for (int j = block_indices[i].lower; j <= block_indices[i].upper; j++)
+        {
+            int *neighbors = find_k_nearest_neighbors(data, data[j], k_max, block_indices, i);
+            for (int l = 0; l < k_max; l++)
+                if (neighbors[l] != -1)
+                    printf("%d %d\n", j, neighbors[l]);
+        }
 
     thread_pool_t *thread_pool = malloc(sizeof(thread_pool_t) + n_threads * sizeof(pthread_t));
 
@@ -234,8 +355,8 @@ int main(int argc, char **argv)
     thread_pool_init(thread_pool, n_threads);
     thread_pool_enqueue(thread_pool, test_function, test_string);
 
-    Task *task = thread_pool_wait(thread_pool);
-    printf("result: %d\n", *((int *)task->result));
+    /* Task *task = thread_pool_wait(thread_pool); */
+    /* printf("result: %d\n", *((int *)task->result)); */
 
     thread_pool_shutdown(thread_pool);
     exit(0);
